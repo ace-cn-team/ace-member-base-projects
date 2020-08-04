9bed1ecdf4982087b381e6164a8dd0ce21b06c52
caspar <279397942@qq.com>
2020-08-04 19:51:02 +0800

    iniit

---
+++ ace-user-base-api-web/src/test/java/ace/user/base/api/web/biz/TestBaseApi.java
@@ -0,0 +1,78 @@
+package ace.user.base.api.web.biz;
+
+import ace.fw.data.model.Sort;
+import ace.fw.data.model.request.resful.PageQueryRequest;
+import ace.fw.data.model.request.resful.WhereRequest;
+import ace.fw.data.model.request.resful.entity.EntityUpdateRequest;
+import ace.fw.util.DateUtils;
+import ace.user.base.api.web.UserBaseApiWebApplication;
+import ace.user.base.api.web.controller.UserBaseController;
+import ace.user.base.define.dao.entity.User;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Arrays;
+
+/**
+ * @author Caspar
+ * @contract 279397942@qq.com
+ * @create 2020/8/4 17:42
+ * @description
+ */
+@Slf4j
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = UserBaseApiWebApplication.class)
+@FixMethodOrder(value = MethodSorters.NAME_ASCENDING)
+public class TestBaseApi {
+    @Autowired
+    private UserBaseController userBaseController;
+
+    @Test
+    public void test_0001_page() {
+        User user = userBaseController.page(PageQueryRequest.builder()
+                .pageIndex(1)
+                .pageSize(1)
+                .sorts(Arrays.asList(
+                        Sort.builder()
+                                .asc(true)
+                                .field(User.ID)
+                                .build()
+                        )
+                )
+                .build()
+        ).check().getData().get(0);
+        String avatarUrl = DateUtils.getNowFormat(DateUtils.FROMAT_yyyyMMddHHmmssSS);
+        String nickName = avatarUrl;
+
+        WhereRequest whereRequest = new WhereRequest();
+        whereRequest.eq("id", user.getId());
+        User modifyUser = User.builder()
+                .nickName(avatarUrl)
+                .avatarUrl(avatarUrl)
+                .build();
+        userBaseController.update(EntityUpdateRequest.<User>builder()
+                .entity(modifyUser)
+                .where(
+                        WhereRequest.build()
+                                .eq("id", user.getId())
+                )
+                .build()
+        ).check();
+        User updateedUser = userBaseController.getById(user.getId()).check();
+
+        Assert.assertEquals(updateedUser.getAvatarUrl(), avatarUrl);
+        Assert.assertEquals(updateedUser.getNickName(), nickName);
+
+        Assert.assertNotEquals(user.getAvatarUrl(), avatarUrl);
+        Assert.assertNotEquals(user.getNickName(), nickName);
+
+        Assert.assertEquals(user.getSignature(), updateedUser.getSignature());
+    }
+}
